a1a63d08a53398c9c29a5e7664d87294af6178d2
Unknown <mvogelmann@mail.csuchico.edu>
2017-04-17 10:47:03 -0700

    week12 monday

---
+++ datalogger.js
@@ -0,0 +1,11 @@
+// Array: can hold varible different numbers, strings etc (information); Area in memory to...
+var numberValues;
+//numberValues = [1,5,9,2];
+
+function addANewNumber()
+{
+  //get text input from the DOM and store in a variable
+  //check if that variable is a number
+  //IF it is, add to the back o fthe Array
+  
+}
